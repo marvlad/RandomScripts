c2b2c3a8bfaf58e9b599e9e49d7dd9b57cda10f4
Marvin V. Ascencio S <marvlad@users.noreply.github.com>
2023-05-21 13:06:15 -0500

    Update Chi2.cpp
    adding the chi2 part

--- Chi2.cpp
+++ Chi2.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-float ptnu_bin[6] = {0.0, 0.5, 1.0, 1.5, 2.0, 4.0};
-
 // Get bin range values
 float get_bin(vector<vector<float> > variable , double lim_0, double lim_1, int verbose){
     float value = 0.0;
@@ -24,50 +22,89 @@ float get_bin(vector<vector<float> > variable , double lim_0, double lim_1, int 
 }
 
 // Reading the files
-vector<vector<float> > read_files(std::string files, bool is_model=false, int verbose = 0){
-    vector<vector<float> > data;
-    vector<float> a, b;
-    float ai, bi;
+vector<vector<float> > read_files(string files, bool is_model=false, int verbose = 0, vector <float> variable = {}){
 
+    // Getting the file
     ifstream file(files.c_str());
+
+    // Make sure that the files can be open
     if (!file) {
         cout << "Error opening file" << endl;
         exit(EXIT_FAILURE);
     }
 
-    while (file >> ai >> bi) {
-        a.push_back(ai);
-        b.push_back(bi);
+    vector<vector<float> > data;
+    vector<vector<float> > model;
+    vector<float> a, b;
+    float ai, bi;
+    string bin_a;
+
+    vector<float > to_return; 
+
+    if(is_model == true){
+        // Getting the values from the path
+        while (file >> ai >> bi) {
+            a.push_back(ai);
+            b.push_back(bi);
+            if(verbose > 3 ) cout << ai << " " << bi << endl;
+            
+        }
+
+        // Addting to the vector of vector
+        data.push_back(a);
+        data.push_back(b);
+
+        // If the file is a model file, we have to do the average 
+        for (int i=0; i < (variable.size() - 1); i++){
+            float variable_value = get_bin(data, variable[i], variable[i+1], verbose); 
+            to_return.push_back(variable_value);
+            if(verbose > 1 ) cout << "bin "<< i << ", value " << variable_value << endl; 
+        }
+
+        model.push_back(to_return);
+        model.push_back(to_return);
+
+        return model;
+    } else {
+        while (file >> bin_a >> ai >> bi) {
+            a.push_back(ai);
+            b.push_back(bi);
+            if(verbose > 3 ) cout << bin_a << " " << ai << " " << bi << endl;
+        }
+        // 'b' is the uncertainty of the measurement 
+        data.push_back(a);
+        data.push_back(b);
+
+        return data;
     }
-    
-    data.push_back(a);
-    data.push_back(b);
+}
 
-    vector<float > model;
-    for (int i=0; i < (sizeof(ptnu_bin)/sizeof(float) - 1); i++){
-        float variable_value = get_bin(data, ptnu_bin[i], ptnu_bin[i+1], verbose); 
-        model.push_back(variable_value);
-        if(verbose > 1 ) cout << "bin "<< i << ", value " << variable_value << endl; 
-    }
+float get_chi2(vector<vector<float> > data, vector<vector<float> > model, int verbose){
+    float chi2 = 0;
+    for (int j = 0; j < data[0].size(); j++){
+        float data_value = data[0][j];
+        float model_value = model[0][j];
+        float chi2_n = (data_value - model_value);
+        float chi2_d = data[1][j];
+        float s_chi2 = chi2_n / chi2_d;
+        if(verbose > 1 ) cout << data_value << " " << model_value << " " << chi2_d << " , s_chi2 " << s_chi2 << endl;  
+        chi2 += s_chi2*s_chi2; 
+    } 
 
-    float a_v = get_bin(data, 0.0, 0.5, verbose);
-
-    cout << "Number of lines: " << a.size() << endl;
-    
-    return data;
+    return chi2;
 }
 
 int main(){
 
-    cout<<" hello Huma"<<endl;
-    
-    vector<vector<float> > data = read_files("/Users/marvinascenciososa/Downloads/chisq/5M_data/ptnu.txt", false, 2);
+    int verbose = 2;
+    string model_path = "/Users/marvinascenciososa/Downloads/chisq/5M_data/ptnu.txt";
+    string data_path = "/Users/marvinascenciososa/Downloads/chisq/E866data/ptnudata.txt";
+    vector <float> ptnu_bin{0.0, 0.5, 1.0, 1.5, 2.0, 4.0};
 
-    cout << "read from here " << endl; 
+    vector<vector<float> > data =  read_files(data_path, false, verbose, ptnu_bin);
+    vector<vector<float> > model = read_files(model_path, true, verbose, ptnu_bin); 
 
-    //for (int j = 0; j < data[0].size(); j++){
-    //    cout << data[0][j] << " " << data[1][j] <<endl;
-    //}
+    cout << get_chi2(data, model, verbose) << endl; 
 
     return 0;
 }
